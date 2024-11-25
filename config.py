3d0e9be3715944b91f01ceb1ece96ae9b0e5b95f
Nachiketa <nachiketapuro@gmail.com>
2024-11-25 19:21:11 +0530

    Added Files of final code

--- config.py
+++
@@ -1,2 +0,0 @@
-CODE_DIR = "/iitjhome/m23csa016/meesho_code"
-DATA_DIR = "/scratch/data/m23csa016/meesho_data"
--- useful_commands.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export PATH="/iitjhome/m23csa016/.conda/envs/meesho/bin:$PATH"
-python /iitjhome/m23csa016/meesho_code/cat_specific_attrs_fine/finetuning.py
